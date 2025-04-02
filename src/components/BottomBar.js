00d5f8d9fed0ce5292a64dc44092fdf0d67d75c5
devyaniverma1998 <devyaniverma5@gmail.com>
2025-04-02 16:30:36 +0530

    code base

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,36 +1,38 @@
-import '../styles/style.css'
-export default function BottomBar(){
-    return(
+import '../styles/style.css';
+import { Link } from 'react-router-dom';
+
+export default function BottomBar() {
+    return (
         <div className="bottom-bar">
-        <div className="container-fluid">
-            <div className="row align-items-center">
-                <div className="col-md-3">
-                    <div className="logo">
-                        <a href="index.html">
-                            <img src="img/logo.png" alt="Logo"/>
-                        </a>
+            <div className="container-fluid">
+                <div className="row align-items-center">
+                    <div className="col-md-3">
+                        <div className="logo">
+                          <Link href="/" >
+                                <img src="img/logo.png" alt="Logo"/>
+                           </Link>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="search">
-                        <input type="text" placeholder="Search"/>
-                        <button><i className="fa fa-search"></i></button>
+                    <div className="col-md-6">
+                        <div className="search">
+                            <input type="text" placeholder="Search"/>
+                            <button><i className="fa fa-search"></i></button>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-3">
-                    <div className="user">
-                        <a href="wishlist.html" className="btn wishlist">
-                            <i className="fa fa-heart"></i>
-                            <span>(0)</span>
-                        </a>
-                        <a href="cart.html" className="btn cart">
-                            <i className="fa fa-shopping-cart"></i>
-                            <span>(0)</span>
-                        </a>
+                    <div className="col-md-3">
+                        <div className="user">
+                          <Link href="/wishlist" className="btn wishlist" >
+                                <i className="fa fa-heart"></i>
+                                <span>(0)</span>
+                           </Link>
+                          <Link href="/cart" className="btn cart" >
+                                <i className="fa fa-shopping-cart"></i>
+                                <span>(0)</span>
+                           </Link>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
